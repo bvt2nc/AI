75b05513f3cc94c7b68a3faeec405a683a8d0e52
Oliver Shi <oys4cv@virginia.edu>
2018-04-30 19:05:48 -0400

    phase 2 a and b

--- HW5/bandit.py
+++ HW5/bandit.py
@@ -14,31 +14,187 @@ import numpy as np
 TEAM_NAME = "idk"
 MEMBERS = ["bvt2nc", 'oys4cv']
 
-def main():
-	alpha = randint(1, 5)
-	beta = randint(1, 5)
-	scale = randint(1, 50)
-	size = 100
-	data = generateData(alpha, beta, scale, size)
-	mean = np.mean(data) # / scale
-	x = 10
-	cdf = ecdf(data, x)
-	mode = getMode(data)
-	var = variance(data, mean) # / (scale ** 2)
-	empA, empB = getParams(mean, var)
+def load_info():
+	f_name = TEAM_NAME + ".json"
+	try:
+		return json.loads(open(f_name).read())
+	except:
+		return {}
 
-	print("alpha: %s" % (alpha))
-	print("beta: %s" % (beta))
-	print("scale: %s" % (scale))
-	print("max: %s" % (max(data)))
-	print("mean: %s" % (mean)) #empirical average
-	print("E[x]: %s" % (betaMean(alpha, beta, scale))) #theoretical average
-	print("Var[x]: %s" % (var)) #empirical variance
-	print("alpha: %s" % (empA))
-	print("beta: %s" % (empB))
-	#print("mode: %s" % (mode))
-	#print("betaMode: " + str(betaMode(alpha, beta, scale)))
-	print("cdf[%s]: %s" % (x, cdf))
+def save_info(info):
+	f_name = TEAM_NAME + ".json"
+	f = open(f_name,"w")
+	f.write(json.dumps(info))
+	f.flush()
+	f.close()
+
+def main():
+	simulate()
+	info = load_info()
+	print(info["bestslots"])
+
+def get_move(state):
+	info = load_info()
+	return get_move_bandit(state, info)
+	#save_info_bandit(state, info)
+	# print(state,info)
+
+
+
+def phase_2_a(state, info):
+	top15 = info['bestslots']
+	return {
+		"team-code": state['team-code'],
+		"game": "phase_2_a",
+		"auctions": [i[0] for i in top15[-10:0]]
+	}
+
+def get_expected_value(top15, auction_number):
+	for number, value in top15:
+		if number == auction_number:
+			return value
+	return 0
+
+def in_top15(top15, auction_number):
+	for number, value in top15:
+		if number == auction_number:
+			return True
+	return False
+
+def phase_2_b(state, info):
+	top15 = info['bestslots']
+	bid = 0
+	if in_top15(top15, state['auction-number']):
+		expected_value = get_expected_value(top15, state['auction-number'])
+		bid = expected_value // 3 + 1
+
+	return {
+		"team-code": state['team-code'],
+		"game": "phase_2_b",
+		"bid": bid
+	}
+
+
+def get_move_bandit(state, info):
+	game_type = state['game']
+	if state["game"] == "phase_1":
+
+		slot = 0
+
+		if "pull-to-gain" in info:
+			if info["pull-to-gain"] is not None:
+				slot = info["pull-to-gain"]
+			elif "last-pull" in info:
+				lastpull = info["last-pull"]
+				slot = lastpull
+				#print(lastpull)
+				if str(lastpull) in info:
+					data = info[str(lastpull)]
+					data.append(state["last-payoff"] - state["last-cost"])
+					if len(data) >= 30:
+						#print("next")
+						slot = slot + 1
+						info[str(slot)] = []
+						if slot >= 100:
+							info["pull-to-gain"] = bestSlot(info)
+							slot = info["pull-to-gain"]
+					else:
+						info[str(slot)] = data
+				else:
+					info[str(lastpull)] = [state["last-payoff"]]
+		else:
+			info["pull-to-gain"] = None
+			info["last-pull"] = 0
+			slot = 0
+
+		info["last-pull"] = slot
+		save_info(info)
+		return {
+			"team-code": state["team-code"],
+			"game": state["game"],
+			"pull": slot,
+		}
+
+	elif state["game"] == "phase_2_a":
+		# auctions = []
+		# return {
+		# 	"team-code": state["team-code"],
+		# 	"game": state["game"],
+		# 	"auctions": auctions
+		# }
+		return phase_2_a(state, info)
+
+	elif state["game"] == "phase_2_b":
+		# bid = 0
+		# return {
+		# 	"team-code": state["team-code"],
+		# 	"game": state["game"],
+		# 	"bid" : bid
+		# }
+		return phase_2_b(state, info)
+
+def bestSlot(info):
+	bestslots = []
+	for i in range(100):
+		total = 0
+		for el in info[str(i)]:
+			total = total + el
+		
+		total = total / 30
+		if len(bestslots) < 15:
+			bestslots.append((i, total))
+			bestslots = sorted(bestslots, key=lambda tup: tup[1])
+		else:
+			if bestslots[0][1] < total:
+				bestslots[0] = (i, total)
+				bestslots = sorted(bestslots, key=lambda tup: tup[1])
+	info["bestslots"] = bestslots
+	save_info(info)
+
+	return bestslots[14][0]
+
+def phase1(slots):
+	ret = None
+	lastcost = None
+	payoff = None
+	credits = 1000000
+	for i in range(3100):
+		print("Round: " + str(i))
+		if ret is None:
+			lastcost = None
+			payoff = None
+		else:
+			alpha = slots[ret["pull"]][0]
+			beta = slots[ret["pull"]][1]
+			scale = slots[ret["pull"]][2]
+			lastcost = slots[ret["pull"]][3]
+			credits = credits - lastcost
+			payoff = np.random.beta(a=alpha, b=beta) * scale
+
+		state = {
+			"team-code": "aaaaaa",
+			"game": "phase_1",
+			"pulls-left": 10000 - i - 1,
+			"last-cost": lastcost,
+			"last-payoff": payoff,
+			"last-metadata": 00000000,
+		}
+
+		ret = get_move(state)
+
+	return credits
+
+def simulate():
+	slots = []
+	for i in range(100):
+		alpha = randint(1, 5)
+		beta = randint(1, 5)
+		scale = randint(2, 10)
+		cost = 3 #randint(2, 10)
+		slots.append((alpha, beta, scale, cost))
+
+	credits = phase1(slots)
+	print("Net: %s" % (1000000 - credits))
 
 def generateData(alpha, beta, scale, size):
 	ret = []
@@ -87,8 +243,32 @@ def getParams(mean, var):
 	beta = alpha * ((1 / mean) - 1)
 	return round(alpha), round(beta)
 
+def test():
+	alpha = randint(1, 5)
+	beta = randint(1, 5)
+	scale = randint(1, 50)
+	size = 30
+	data = generateData(alpha, beta, scale, size)
+	mean = np.mean(data) #/ scale
+	x = 10
+	cdf = ecdf(data, x)
+	mode = getMode(data)
+	var = variance(data, mean) #/ (scale ** 2)
+	empA, empB = getParams(mean, var)
 
-
+	print("alpha: %s" % (alpha))
+	print("beta: %s" % (beta))
+	print("scale: %s" % (scale))
+	print("max: %s" % (max(data)))
+	print("mean: %s" % (mean)) #empirical average
+	print("E[x]: %s" % (betaMean(alpha, beta, scale))) #theoretical average
+	print("Error: %s" % (abs(mean - betaMean(alpha, beta, scale)) / betaMean(alpha, beta, scale) * 100))
+	print("Var[x]: %s" % (var)) #empirical variance
+	print("alpha: %s" % (empA))
+	print("beta: %s" % (empB))
+	#print("mode: %s" % (mode))
+	#print("betaMode: " + str(betaMode(alpha, beta, scale)))
+	print("cdf[%s]: %s" % (x, cdf))
 
 if __name__=='__main__':
 	main()
